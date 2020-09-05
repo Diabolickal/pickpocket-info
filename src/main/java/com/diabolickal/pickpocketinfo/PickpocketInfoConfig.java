9f225183ad9f134bb1d666d4e905328610a0d8a9
Nathan <nrdugan2010@gmail.com>
2020-09-05 14:40:36 -0500

    Initial Commit

---
+++ src/main/java/com/diabolickal/pickpocketinfo/PickpocketInfoConfig.java
@@ -0,0 +1,94 @@
+package com.diabolickal.pickpocketinfo;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Range;
+
+@ConfigGroup("Pickpocket Info")
+public interface PickpocketInfoConfig extends Config
+{
+
+    @ConfigItem(
+            keyName = "showDodgy",
+            name = "Show Dodgy Necklace Info",
+            description = "Whether or not to display Dodgy Necklace charges.",
+            position = 0
+    )
+    default boolean showDodgy()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "showPouches",
+            name = "Show Pouches Counter",
+            description = "Whether or not to display the number pouches in your inventory on the overlay.",
+            position = 1
+    )
+    default boolean showPouches()
+    {
+        return true;
+    }
+
+    @Range(
+            min = 1,
+            max = 10
+    )
+    @ConfigItem(
+            keyName = "warnThreshold",
+            name = "Warning Threshold",
+            description = "At how many dodgy necklace charges should the overlay text turn red.",
+            position = 2
+    )
+    default int warnThreshold(){ return 1;}
+
+    @Range(
+            max = 30
+    )
+    @ConfigItem(
+            keyName = "overlayDuration",
+            name = "Overlay Duration",
+            description = "How long the overlay lasts between pickpockets in seconds. Zero means overlay will never go away.",
+            position = 3
+    )
+    default int overlayDuration(){ return 10;}
+
+    @ConfigItem(
+            keyName = "resetType",
+            name = "Reset Rate on",
+            description = "When to reset the timer. On logout or upon exiting Runelite.",
+            position = 4
+    )
+    default ResetType resetType()
+    {
+        return ResetType.EXIT;
+    }
+
+
+
+    /*===HIDDEN===*/
+
+    //Keeps track of dodgy necklace charges even if Runelite is closed
+    @ConfigItem(
+            keyName = "dodgyNecklace",
+            name = "",
+            description = "",
+            hidden = true
+    )
+    default int dodgyNecklace()
+    {
+        return -1;
+    }
+
+    @ConfigItem(
+            keyName = "dodgyNecklace",
+            name = "",
+            description = ""
+    )
+    void dodgyNecklace(int dodgyNecklace);
+
+
+}
+
+
---
+++ src/main/java/com/diabolickal/pickpocketinfo/PickpocketInfoOverlay.java
@@ -0,0 +1,77 @@
+package com.diabolickal.pickpocketinfo;
+
+import net.runelite.api.Client;
+import net.runelite.client.ui.overlay.OverlayMenuEntry;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.time.Duration;
+import java.time.Instant;
+
+import static net.runelite.api.MenuAction.RUNELITE_OVERLAY_CONFIG;
+import static net.runelite.client.ui.overlay.OverlayManager.OPTION_CONFIGURE;
+
+public class PickpocketInfoOverlay extends OverlayPanel
+{
+
+    private final Client client;
+    private final PickpocketInfoPlugin plugin;
+    private final PickpocketInfoConfig config;
+
+    @Inject
+    PickpocketInfoOverlay(Client client, PickpocketInfoPlugin plugin, PickpocketInfoConfig config)
+    {
+        super(plugin);
+        this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+        setPosition(OverlayPosition.TOP_LEFT);
+        getMenuEntries().add(new OverlayMenuEntry(RUNELITE_OVERLAY_CONFIG, OPTION_CONFIGURE, "PP overlay"));
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics)
+    {
+        String charges = "";
+        if(plugin.dodgyCharges() >= 0)
+            charges = ""+plugin.dodgyCharges();
+        if(!plugin.hasDodgy())
+            charges = "";
+
+        if(plugin.lastPickpocket() != null && (config.overlayDuration() <1  || Duration.between(plugin.lastPickpocket(), Instant.now()).getSeconds() < config.overlayDuration()))
+        {
+            panelComponent.getChildren().add(TitleComponent.builder()
+                    .text("Pickpocketing")
+                    .color(Color.WHITE)
+                    .build());
+            if(config.showDodgy())
+            {
+                panelComponent.getChildren().add(LineComponent.builder()
+                        .left(!plugin.hasDodgy() ? "No Dodgy" : "Dodgy Charges")
+                        .leftColor(plugin.dodgyCharges() <= config.warnThreshold() || !plugin.hasDodgy() ? Color.RED : Color.WHITE)
+                        .right(charges)
+                        .rightColor(plugin.dodgyCharges() <= config.warnThreshold() && plugin.hasDodgy() ? Color.RED : Color.WHITE)
+                        .build());
+            }
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Rate")
+                    .right(String.format("%.1f",plugin.percent())+"%")
+                    .build());
+            if(config.showPouches() && plugin.targetHasPouches())
+            {
+                panelComponent.getChildren().add(LineComponent.builder()
+                        .left("Pouches")
+                        .leftColor(plugin.pouchNum() >= 27 ? Color.RED : Color.WHITE)
+                        .right(""+plugin.pouchNum())
+                        .rightColor(plugin.pouchNum() >= 27 ? Color.RED : Color.WHITE)
+                        .build());
+            }
+
+        }
+        return super.render(graphics);
+    }
+}
